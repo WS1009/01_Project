17775456ed02b4919c5a552d080aa3e1c2bb77e3
wangshun <wangshun611@163.com>
2017-07-26 22:50:02 +0800

    maven_crm
    maven+ssh

---
+++ maven_crm/src/main/java/cn/itcast/service/UserService.java
@@ -0,0 +1,37 @@
+package cn.itcast.service;
+
+import java.util.List;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import cn.itcast.dao.UserDao;
+import cn.itcast.dao.UserDaoImpl;
+import cn.itcast.entity.User;
+
+/** 
+ *
+ * @author  WS
+ * @date 创建时间：2017-6-5 下午9:06:46 
+ * @version 1.0 
+ */
+@Transactional
+public class UserService {
+	private UserDao  userDao;
+
+	public void setUserDao(UserDao userDao) {
+		this.userDao = userDao;
+	}
+
+	public User login(User user) {
+		//调用dao里面的方法
+		return userDao.loginUser(user);
+	}
+
+	public List<User> findAll() {
+		return userDao.findAll();
+	}
+
+
+	
+
+}
